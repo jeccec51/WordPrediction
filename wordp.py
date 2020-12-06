fad9dfc1620704028c85656fec239a953a0c68ec
Sreejith Markkasseri <sreejith.markkassery@gmail.com>
2020-12-07 01:06:50 +0530

    Added the model structure
    Added the model structure. Net work class is defined, and forward function is developed

--- wordp.py
+++ wordp.py
@@ -101,4 +101,26 @@ def similarity(embedding, valid_size=16, valid_window=100, device='cpu'):
     similarities = torch.mm(valid_vectors, embed_vectors.t()) / magnitudes
     return valid_examples, similarities
 
+# ____________________________________________Define the model
+
+
+class SkipGram(nn.Module):
+    def __init__(self, n_vocab, n_embed):
+        super().__init__()
+
+        self.embed = nn.Embedding(n_vocab, n_embed)
+        self.output = nn.Linear(n_embed, n_vocab)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = self.embed(x)
+        scores = self.output(x)
+        log_ps = self.log_softmax(scores)
+        return log_ps
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Training
+
+
+
+
 
